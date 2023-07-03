33bc87e017eb4a891e2fc853b45b2b17bc70d2f0
Luis Pomare <luispomare92@gmail.com>
2023-07-03 09:53:32 -0500

    Fix models to use correct relationships' names

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :entities
-  has_many :groups
+  has_many :entity
+  has_many :group
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_03_115442) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_03_143531) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_03_115442) do
     t.float "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.bigint "group_id", null: false
+    t.index ["group_id"], name: "index_entities_on_group_id"
+    t.index ["user_id"], name: "index_entities_on_user_id"
   end
 
   create_table "groups", force: :cascade do |t|
@@ -26,6 +30,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_03_115442) do
     t.jsonb "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,4 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_03_115442) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "entities", "groups"
+  add_foreign_key "entities", "users"
+  add_foreign_key "groups", "users"
 end
