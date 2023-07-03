94d2bd1e699412e235b898111457fb96fe4d1144
Luis Pomare <luispomare92@gmail.com>
2023-07-03 11:04:59 -0500

    Modify index controllers' actions to render only current user's entities

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,7 +3,7 @@ class GroupsController < ApplicationController
 
   # GET /groups or /groups.json
   def index
-    @groups = Group.all
+    @groups = Group.where(user: current_user)
   end
 
   # GET /groups/1 or /groups/1.json
