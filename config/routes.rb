1abd9829ddfe50019c52b42dd79a53e49dce1c94
Luis Pomare <luispomare92@gmail.com>
2023-07-06 09:33:14 -0500

    Create custom route for new log_in path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :users do
+    get 'log_in', to: 'devise/sessions#log_in'
+  end
+
   resources :groups
   resources :entities
   root "groups#index"
